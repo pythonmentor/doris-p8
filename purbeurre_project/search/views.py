881a1a9ea3413608b7a6cf0d0db604352c967224
dorisak <dorisatchikiti@MacBook-Pro-de-doris.local>
2018-11-18 16:53:48 +0900

    autocomplete réalisé

--- purbeurre_project/search/views.py
+++ purbeurre_project/search/views.py
@@ -9,17 +9,28 @@ import json
 def search_function(request):
     """ Get the user input of products and search unhealthy propositions in
      the database """
-    if request.method == 'POST':
+    if request.method == 'GET':
         query = request.GET.get('term', '')
-        print(query)
-        unhealthy_prod = Product.objects.filter(name_prod__icontains = query)
+        unhealthy_prod = Product.objects.filter(
+            nutrition_grade__range = ('d','e'), name_prod__icontains = query)
         results = []
 
         for prod in unhealthy_prod:
             results.append(prod.name_prod)
         data = json.dumps(results)
-    else:
-        data = 'fail'
-    mimetype = 'application/json'
+        mimetype = 'application/json'
 
-    return HttpResponse(data, mimetype)
+        return HttpResponse(data, mimetype)
+
+# def get_results(request):
+#     """ Get and display healthy results """
+#     if request.method == 'GET':
+#         # choices = request.GET.get('search','None')
+#         print('ok')
+#         # unhealthy = search_function()
+#         # print('hello')
+#         # test = unhealthy.Category.all()
+#         #
+#         # cat = print(test)
+#
+#         return render(request, 'search/search.html')
